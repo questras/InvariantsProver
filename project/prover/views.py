7192f86c596fb6f42ab561e7e6abf0226180c2f3
questras <michu123qwe@gmail.com>
2021-04-27 11:46:20 +0200

    Add file opening, showing only user's dirs and files.

--- project/prover/views.py
+++ project/prover/views.py
@@ -5,47 +5,67 @@ from django.views.generic import TemplateView
 from .models import Directory, File
 
 
-# TODO: style for go back button
+def get_file_content(file):
+    file.open('r')
+    content = file.read()
+    file.close()
+
+    return content
+
+
 class MainView(TemplateView):
     template_name = 'main.html'
 
-    def get_current_directory(self):
-        """Return current working directory specified by
-        ID in url query string, or None if no directory specified or
-        query string is incorrect"""
+    def _get_current_object(self, cls, phrase):
+        obj = None
 
-        directory = None
-        if 'dir' in self.request.GET:
+        if self.request.user.is_authenticated and phrase in self.request.GET:
+            pk = self.request.GET[phrase]
             try:
-                directory = Directory.objects.get(pk=self.request.GET['dir'])
-            except Directory.DoesNotExist:
-                directory = None
+                obj = cls.objects.get(pk=pk, owner=self.request.user)
+            except cls.DoesNotExist:
+                obj = None
             except ValueError:
                 # Instead of ID, got something else.
-                directory = None
+                obj = None
 
-        return directory
+        return obj
+
+    def _get_current_objects(self, cls, parent_dir: Directory = None):
+        result = None
+        if self.request.user.is_authenticated:
+            result = cls.objects.filter(parent_dir=parent_dir, owner=self.request.user)
+
+        return result
+
+    def get_current_file(self):
+        return self._get_current_object(File, 'file')
+
+    def get_current_directory(self):
+        return self._get_current_object(Directory, 'dir')
 
     def get_directories_to_show(self, parent_dir: Directory = None):
-        """Get directories to show in view that are in given
-        parent_dir or are in main directory, if parent_dir is None."""
-
-        return Directory.objects.filter(parent_dir=parent_dir)
+        return self._get_current_objects(Directory, parent_dir=parent_dir)
 
     def get_files_to_show(self, parent_dir: Directory = None):
-        """Get files to show in view that are in given
-        parent_dir or are in main directory, if parent_dir is None."""
-
-        return File.objects.filter(parent_dir=parent_dir)
+        return self._get_current_objects(File, parent_dir=parent_dir)
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         current_directory = self.get_current_directory()
+        current_file = self.get_current_file()
         directories = self.get_directories_to_show(current_directory)
         files = self.get_files_to_show(current_directory)
+
+        if current_file:
+            file_content = get_file_content(current_file.file)
+        else:
+            file_content = None
 
         context = super().get_context_data(**kwargs)
         context['directories'] = directories
         context['files'] = files
         context['current_dir'] = current_directory
+        context['current_file'] = current_file
+        context['file_content'] = file_content
 
         return context
