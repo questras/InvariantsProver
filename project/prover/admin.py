a5d8c9337d1370fdd4c3792f3e95bb82b8143349
questras <michu123qwe@gmail.com>
2021-04-30 11:19:29 +0200

    Add display classes for file and directory to admin.

--- project/prover/admin.py
+++ project/prover/admin.py
@@ -9,8 +9,18 @@ from .models import (
     FileSection
 )
 
-admin.site.register(Directory)
-admin.site.register(File)
+
+class AdminDirectory(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'parent_dir', 'availability_flag')
+
+
+class AdminFile(admin.ModelAdmin):
+    list_display = ('id', 'get_name', 'owner',
+                    'parent_dir', 'availability_flag')
+
+
+admin.site.register(Directory, AdminDirectory)
+admin.site.register(File, AdminFile)
 admin.site.register(SectionCategory)
 admin.site.register(SectionStatusData)
 admin.site.register(SectionStatus)
